a32e9f9460c9db393b59552b5733ccb7553733b2
absrdprogrammer <112478575+absrdprogrammer@users.noreply.github.com>
2022-11-08 13:59:49 +0700

    Add files via upload

---
+++ pj-kelompok.py
@@ -0,0 +1,11 @@
+def rumus_diskon(harga, diskon):
+    diskon = int(diskon / 100 * harga)
+    jumlah_bayar = int(harga - diskon)
+    print("===============Hasil=================")
+    print("Harga barang :", harga)
+    print("Total diskon :", diskon)
+    print("Jumlah yg harus dibayar :", jumlah_bayar)
+
+harga = int(input("Harga barang :"))
+diskon = int(input("Total diskon (dalam persen, ex : 5) : "))
+rumus_diskon(harga, diskon)
