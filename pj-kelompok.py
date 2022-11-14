c68cfb1dd7bb4f1b9f0e46ae70071da84da2b0cb
absrdprogrammer <112478575+absrdprogrammer@users.noreply.github.com>
2022-11-14 08:19:13 +0000

    deret part 2

--- pj-kelompok.py
+++ pj-kelompok.py
@@ -7,12 +7,26 @@ def rumus_diskon(harga, diskon):
     print("Total diskon :", diskon)
     print("Jumlah yg harus dibayar :", jumlah_bayar)
 
+def barisan_deret():
+    fitur = ["Barisan", "Deret"]
+    for i in range(2):
+        print(f"{i+1}. {fitur[i]}")
+    pilihan = input("Input nomor program yang akan dipakai : ")
+    if pilihan=="1":
+        list = (input("Silahkan input barisan, pisahkan dengan spasi. (Contoh : 1 2 3 4 5) : ").split(" "))
+        suku = int(input("Silahkan input suku yg ingin diketahui. (Contoh : 25) : "))
+        a = float(list[0])
+        b = float(list[1]) - float(list[0])
+        hasil = a + (suku - 1) * b
+        print(f"Suku ke-{suku} dari barisan tersebut adalah {hasil}")
+    elif pilihan=="2":
+        list = (input("Silahkan input deret, pisahkan dengan spasi. (Contoh : 1 2 3 4 5) : ").split(" "))
+        suku = int(input("Silahkan input jumlah dari berapa suku pertama yg ingin diketahui. (Contoh : 25) : "))
+        a = float(list[0])
+        b = float(list[1]) - float(list[0])
+        hasil = float(suku / 2 * (2* a + (suku - 1) * b))
+        print(f"Jumlah {suku} suku pertama dari deret tersebut adalah {hasil}")
+    else:
+        print("Perintah tidak ditemukan. Program exit.")
 
-# choice = "barisan"
-# if choice=="barisan":
-#     list = (input("Silahkan input barisan, pisahkan dengan spasi. (Contoh : 1 2 3 4 5) : ").split(" "))
-#     suku = int(input("Silahkan input suku yg ingin diketahui. (Contoh : 25) : "))
-#     a = int(list[0])
-#     b = int(list[1]) - int(list[0])
-#     hasil = a + (suku - 1) * b
-#     print(f"Suku ke-{suku} dari barisan tersebut adalah {hasil}")
+barisan_deret()
