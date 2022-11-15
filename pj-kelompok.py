55b293d885a79e06edd7e9295d6d0fbf97bcbc51
absrdprogrammer <112478575+absrdprogrammer@users.noreply.github.com>
2022-11-15 12:14:09 +0700

    Add files via upload

---
+++ pj-kelompok.py
@@ -0,0 +1,78 @@
+def rumus_diskon(harga, diskon):
+    """Rumus Diskon"""
+    diskon = int(diskon / 100 * harga)
+    jumlah_bayar = int(harga - diskon)
+    print("===============Hasil=================")
+    print("Harga barang :", harga)
+    print("Total diskon :", diskon)
+    print("Jumlah yg harus dibayar :", jumlah_bayar)
+
+def barisan_deret():
+    fitur = ["Barisan", "Deret"]
+    for i in range(2):
+        print(f"{i+1}. {fitur[i]}")
+    pilihan = input("Input nomor program yang akan dipakai : ")
+    if pilihan=="1":
+        list = (input("Silahkan input barisan, pisahkan dengan spasi. (Contoh : 1 2 3 4 5) : ").split(" "))
+        suku = int(input("Silahkan input suku yg ingin diketahui. (Contoh : 25) : "))
+        a = float(list[0])
+        b = float(list[1]) - float(list[0])
+        hasil = a + (suku - 1) * b
+        print(f"Suku ke-{suku} dari barisan tersebut adalah {hasil}")
+    elif pilihan=="2":
+        list = (input("Silahkan input deret, pisahkan dengan spasi. (Contoh : 1 2 3 4 5) : ").split(" "))
+        suku = int(input("Silahkan input jumlah dari berapa suku pertama yg ingin diketahui. (Contoh : 25) : "))
+        a = float(list[0])
+        b = float(list[1]) - float(list[0])
+        hasil = float(suku / 2 * (2* a + (suku - 1) * b))
+        print(f"Jumlah {suku} suku pertama dari deret tersebut adalah {hasil}")
+    else:
+        print("Perintah tidak ditemukan. Program exit.")
+
+#############################Silmi#####################################
+print('Rumus kecepatan\n')
+print('+'*100, '\n')
+
+def kecepatan():
+    jarak = float(input("Masukkan jarak dalam meter : "))
+    waktu = float(input("Masukkan waktu dalam detik : "))
+    kecepatan = jarak/waktu
+    print("Kecepatan kelereng menggelinding adalah : ", kecepatan, "m/s\n")
+
+def jarak():
+    kecepatan = float(input("Masukkan kecepatan dalam m/s : "))
+    waktu = float(input("Masukkan waktu dalam detik : "))
+    jarak = kecepatan*waktu
+    print("Jarak tempuh kelereng adalah : ", jarak, "m\n")
+
+def waktu():
+    jarak = float(input("Masukkan jarak dalam meter : "))
+    kecepatan = float(input("Masukkan kecepatan dalam m/s : "))
+    waktu = jarak/kecepatan
+    print("Waktu tempuh kelereng adalah : ", waktu, "s\n")
+
+
+def menu():
+    print("Menu")
+    print("1.Kecepatan")
+    print("2.Jarak")
+    print("3.Waktu")
+    print("4.Selesai")
+
+loop = True
+while loop:
+    menu()
+    masukan = input("Masukkan pilihan : ")
+    if masukan == "1":
+        kecepatan()
+    elif masukan == "2":
+        jarak()
+    elif masukan == "3":
+        waktu()
+    elif masukan == "4":
+        print("Program selesai")
+        loop = False
+    else:
+        print("Input Invalid")
+
+######################################################################
